22a2daec3bf606d557efd187ede72226316f7904
Steffen Steinbrecher <steffen.steinbrecher@gmail.com>
2015-08-02 17:17:22 +0200

    Added License-Information to Source-Files

--- YAHW/BaseClasses/ViewModelBase.cs
+++ YAHW/BaseClasses/ViewModelBase.cs
@@ -1,4 +1,32 @@
-﻿using YAHW.MVVMBase;
+﻿// YAHW - Yet Another Hardware Monitor
+// Copyright (c) 2015 Steffen Steinbrecher
+// Contact and Information: http://csharp-blog.de/category/yahw/
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.If not, see<http://www.gnu.org/licenses/>.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// THIS COPYRIGHT NOTICE MAY NOT BE REMOVED FROM THIS FILE
+
+using YAHW.MVVMBase;
 
 namespace YAHW.BaseClasses
 {
@@ -21,23 +49,5 @@ namespace YAHW.BaseClasses
     /// </summary>
     public abstract class ViewModelBase : BindableBase
     {
-        //private DependencyFactory dependencyFactory;
-
-        ///// <summary>
-        ///// Dependency factory the DI-Container
-        ///// </summary>
-        //public DependencyFactory DependencyFactory
-        //{
-        //    get { return dependencyFactory; }
-        //    private set { dependencyFactory = value; }
-        //}
-
-        ///// <summary>
-        ///// Standard CTOR
-        ///// </summary>
-        //public ViewModelBase()
-        //{
-        //    this.DependencyFactory = new DependencyFactory();
-        //}
     }
 }
